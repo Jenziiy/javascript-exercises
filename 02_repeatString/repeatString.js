99107e85be19a4848d83257efdbce1ba42406138
user.email <developerjenzii@gmail.com>
2022-06-30 01:32:54 +0200

    Change code to pass testcases in jest

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+let string = ''; //  = prompt("insert a string", "hey");
+let num; // = prompt("insert a number");
 
-};
+
+
+const repeatString = function(string, num) {
+  let text = '';
+  let i = 0;
+while( i < num)
+{
+  text += string;
+  i++;
+}
+if (num < 0){
+  text += 'ERROR';
+}
+return text;
+
+ };
 
 // Do not edit below this line
 module.exports = repeatString;
