5bc152ddc2cfe23131c911cd4418c69d0d98d96f
user.email <developerjenzii@gmail.com>
2022-07-03 20:54:57 +0200

    Solve removeFromArray problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,4 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(...args) { return args[0].filter(arg => !args.includes(arg))};
 
 // Do not edit below this line
 module.exports = removeFromArray;
