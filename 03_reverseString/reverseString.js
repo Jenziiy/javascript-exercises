3a1438dc30bfd53e0f8a6a406da0f7aaea142898
user.email <developerjenzii@gmail.com>
2022-07-03 14:18:18 +0200

    Reverse string and execute testsuite TDD

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(regText) {
+const reverseArray = regText.split('');
+return reverseText = reverseArray.reverse().join('');
 };
 
 // Do not edit below this line
