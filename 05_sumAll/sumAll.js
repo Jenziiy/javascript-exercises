5dc9792a92a4ab90951e18972b43d0feb9d6dbdd
user.email <developerjenzii@gmail.com>
2022-07-03 22:42:49 +0200

    sum all incoming integers and pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+  let sum = 0;
+  args.sort();
+  if (args[0] >= 0 && args[1] >= 0 && typeof args[0] && typeof args[1] == "number" ) {
+    for(let i = args[0]; i <= args[1] ; i++) {
+      sum += i;
+  }
+  } else {
+    return sum = 'ERROR';
+    }
+return sum;
 };
 
 // Do not edit below this line
