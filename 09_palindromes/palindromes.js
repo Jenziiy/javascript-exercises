e5b34dafa2949ab665d169ca3e7429252a9aac06
user.email <developerjenzii@gmail.com>
2022-07-26 00:24:24 +0200

    finish palindrome check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+let normalString = input.toLowerCase().replace(/[^a-z]/g, "");
+return normalString.split("").reverse().join("") == normalString;
 };
 
 // Do not edit below this line
