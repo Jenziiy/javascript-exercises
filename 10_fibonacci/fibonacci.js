4330c571fda18fe8078e92e7a22d12692445556e
user.email <developerjenzii@gmail.com>
2022-07-28 00:45:38 +0200

    Write Fibonacci loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(positionOfNumber) {
+  let previousNumber = 0;
+  let nextNumber = 1; 
 
+  if ( positionOfNumber == 0) { return 0 } 
+    else if (positionOfNumber < 0) { return 'OOPS'};
+
+    for(let i = 1; i < positionOfNumber; i++){  
+      const glassHalfEmpty = nextNumber;
+      nextNumber = previousNumber + nextNumber
+      previousNumber = glassHalfEmpty;
+    }
+
+  return nextNumber;
 };
 
 // Do not edit below this line
