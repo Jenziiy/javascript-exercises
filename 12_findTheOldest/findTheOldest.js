1e0ba0ee8f307df2bf1db94e6d9827697952126d
user.email <developerjenzii@gmail.com>
2022-07-28 23:17:21 +0200

    Use reduce to find oldest age between prev, next.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((personA, personB) => {
+    const personaAge = getAge(personA.yearOfBirth, personA.yearOfDeath);
+    const personbAge = getAge(personB.yearOfBirth, personB.yearOfDeath);
+    return personaAge < personbAge ? personB : personA;
+  })
 };
+
+const getAge = function(birthYear, deathYear) {
+  if(!deathYear) {
+    deathYear = new Date().getFullYear();
+  }
+  return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
