afe94bdce4c8f05f5df2fa8bdf71254db1cb6730
user.email <developerjenzii@gmail.com>
2022-07-29 16:43:45 +0200

    Let variable names better reflect function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,8 @@
 const findTheOldest = function(people) {
-  return people.reduce((personA, personB) => {
-    const personaAge = getAge(personA.yearOfBirth, personA.yearOfDeath);
-    const personbAge = getAge(personB.yearOfBirth, personB.yearOfDeath);
-    return personaAge < personbAge ? personB : personA;
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return oldestAge < currentAge ? currentPerson : oldestPerson;
   })
 };
 
